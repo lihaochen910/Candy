dd867f839822be5f2b4b96a1f64ed3e0a432b0f7
Kanbaru <lihaochen910@hotmail.com>
2018-07-24 18:46:45 +0800

    main.py

--- main.py
+++ main.py
@@ -8,14 +8,7 @@ from core.EditorApp import app
 
 from packages.WelcomeScreen import *
 
-def main():
-	WelcomeScreen.launch()
 
-	aku = getAKU()
-	aku.resetContext()
-	aku.setInputConfigurationName('CANDY')
-	aku.runString("print('Hi, Moai')")
+WelcomeScreen.launch()
 
-	app.run()
-
-main()
+app.run()
