587ed3de87bf77f27c20a12e0b52fe7ed752e9fe
Kanbaru <lihaochen910@hotmail.com>
2018-09-23 10:10:02 +0800

    modify

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ from core.EditorModule import EditorModuleManager
 from core.EditorApp import app
 
 from packages.WelcomeScreen import *
-from core import Project
 
 # import logging
 # logging.getLogger().setLevel(logging.INFO)
 
 # WelcomeScreen.launch()
 
-Project.get().load('../test')
+app.openProject('./testProject')
 
 app.run()
